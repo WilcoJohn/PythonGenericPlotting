663d2de675c7514bdd2db82ee19af45bf07676ce
WilcoJohn <60872794+WilcoJohn@users.noreply.github.com>
2024-07-29 08:05:26 +0200

    Update dataGenericPlotting.py
    Expanded "getRelevantFilePaths" function to use with a list or tuple.

--- dataGenericPlotting.py
+++ dataGenericPlotting.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 
-Python code file for data analysis 
+Python code file for generic data plotting and file filtering 
+
 """
 import numpy
 import pandas
@@ -13,33 +14,53 @@ import fnmatch
 
 
 
-
-
 def getRelevantFilePaths(path, pattern, matchPattern = True):       # get relevant files according to a pattern
     results = [];
-    for root, dirs, files in os.walk(path):
-        for fi in files:
-            filePathName = root + '\\' + fi;
-            if (fnmatch.fnmatch(filePathName,pattern) and matchPattern):
-                results.append(filePathName);                   # if pattern matches append file
+    
+    if (type(path) is str):
+        for root, dirs, files in os.walk(path):
+            for fi in files:
+                filePathName = root + '\\' + fi;
+                if (fnmatch.fnmatch(filePathName,pattern) and matchPattern):
+                    results.append(filePathName);                   # if pattern matches append file
                 
-            if (not fnmatch.fnmatch(filePathName,pattern) and not matchPattern):
-                results.append(filePathName);                   # if pattern does not match, append file
+                if (not fnmatch.fnmatch(filePathName,pattern) and not matchPattern):
+                    results.append(filePathName);                   # if pattern does not match, append file
+    
+    elif (type(path) is list or tuple):                             # filter iteratible list for relevant patter
+        if (matchPattern): 
+            results = fnmatch.filter(path, pattern);
+        else: 
+            for pi in path:
+                if (not fnmatch.fnmatch(pi,pattern)):
+                    results.append(pi);
+    else:
+        return "error";
         
     results.sort();
     return results;
 
 
 
-filePath = rf"C:\Users\wilco\OneDrive - Resonant Group\Desktop\Projects\13450 - DEEP\Data Analysis";           # raw string with string interpolation
-filePattern = "*2024-07-22*.csv";   # file patterns to list
+
+
+
+
+
+filePath = r"C:\Users\Test\File\For\User";           # raw string with string interpolation
+filePattern = "*.csv";                               # file patterns to list
 
 relevantFilePaths = getRelevantFilePaths(filePath,filePattern);
 dataRaw = pandas.read_csv(relevantFilePaths[0],skiprows=1);
 
+
 dataPlot = dataRaw[['Time_min','Sensor1','Sensor2','Sensor3','Sensor4']].rolling(window = 60, 
            center = True).mean().dropna();  # do a middle moving average to filter data
                                             # and get rid of unnecesary data
+                                            
+                                            
+                                            
+                                            
                                             
                                             
 F = plt.figure(figsize=(7, 4));
@@ -72,3 +93,7 @@ plt.legend(loc = [1.05,0.65]);
 plt.ylabel('yVal [--]');
 plt.xlabel('xVal [--]');
 plt.show();                             
+
+
+
+
